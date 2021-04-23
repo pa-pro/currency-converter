edbfe23e96335c30bb4a2b8381a10704f0e38a53
Patrycja Osuch <patrycja.osuch@gmail.com>
2021-04-23 17:19:45 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+console.log("Cześć!");
+
+let currencyElement = document.querySelector(".js-currency");
+let plnElement = document.querySelector(".js-pln");
+let formElement = document.querySelector(".js-form");
+let outcomeElement = document.querySelector(".js-outcome");
+
+
+let rateGBP = 5.25;
+let rateEUR = 4.56;
+
+
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+    let pln = +plnElement.value;
+    let currency = currencyElement.value;
+    let outcome;
+
+    switch (currency) {
+
+        case "EUR":
+            outcome = pln / rateEUR;
+
+            break;
+
+        case "GBP":
+            outcome = pln / rateGBP;
+
+            break;
+
+    }
+    
+    outcomeElement.innerHTML = `${pln.toFixed(2)} PLN = ${outcome.toFixed(2)} ${currency}`;
+
+});
